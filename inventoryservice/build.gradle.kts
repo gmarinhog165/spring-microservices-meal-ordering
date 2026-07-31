54996e89d8ea4c78ff758be51387e9bc6b03b2d0
Gonçalo Marinho <a90969@alunos.uminho.pt>
2026-07-31 21:57:50 +0100

    Add actuator health endpoints to all services

--- inventoryservice/build.gradle.kts
+++ inventoryservice/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:3.0.3")
 	compileOnly("org.projectlombok:lombok")
